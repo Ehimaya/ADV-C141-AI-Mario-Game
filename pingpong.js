a1d8ed844034fd42a3c6dec1f70caf3294fe9d43
Ehimaya M <76644164+Ehimaya@users.noreply.github.com>
2022-01-11 21:26:59 +0530

    Add files via upload

---
+++ pingpong.js
@@ -0,0 +1,43 @@
+rightWristX = "";
+rightWristY = "";
+scoreRightWrist = 0;
+
+function setup() {
+	canvas = createCanvas(800,400);
+	canvas.parent('canvas');
+
+	video = createCapture(VIDEO);
+	video.size(800,400);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+
+function gotPoses(results) {
+	if(results.length > 0) {
+		rightWristX = results[0].pose.rightWrist.x;
+	    rightWristY = results[0].pose.rightWrist.y;
+	    console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+        
+		scoreRightWrist =  results[0].pose.keypoints[10].score;
+		console.log("scoreRightWrist = " + scoreRightWrist);
+	
+	}
+}
+
+function draw() {
+    image(video, 0, 0, 800, 400);
+
+	if(scoreRightWrist > 0.2) {
+
+	fill("#00FF00");
+	stroke("#00FF00");
+	circle(rightWristX, rightWristY, 20);
+}
+
+}
